cd232d2544f87e52197157aa5af91462748b1be1
manojringole <manojringole@gmail.com>
2022-09-02 21:33:20 -0700

    UMA-5 : Create below packages in the project

--- src/main/java/com/BikkadIT/UserManagementApp/ServletInitializer.java
+++ src/main/java/com/BikkadIT/UserManagementApp/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.BikkadIT;
+package com.BikkadIT.UserManagementApp;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/main/java/com/BikkadIT/UserManagementApp/UserManagementAppApplication.java
+++ src/main/java/com/BikkadIT/UserManagementApp/UserManagementAppApplication.java
@@ -1,4 +1,4 @@
-package com.BikkadIT;
+package com.BikkadIT.UserManagementApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
